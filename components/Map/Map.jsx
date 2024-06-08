c494522be7b369f8834002bd60b71d1e6676ff50
mainavinhoon <navinrawat11032004@gmail.com>
2024-06-08 09:25:36 +0530

    Maps Overlapping fixed

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -25,8 +25,8 @@ const Map = () => {
     <div id='Map'>
       <div className="text-2xl font-medium py-4 underline">Map</div>
       <div>All events here in map</div>
-      <div className=' mt-3 h-96 w-full'>
-        <MapContainer center={center} zoom={zoomLevel} style={{ height: '100%', width: '100%' }}>
+      <div className=' z-0 relative mt-3 h-96 w-full'>
+        <MapContainer  center={center} zoom={zoomLevel} style={{ height: '100%', width: '100%' }}>
           <TileLayer
             url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=6hQHdjxKLOCvbPfNAr35"
             attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
